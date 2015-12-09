d5abb7857c26ea4be0380d301f6d509ce4ea28f4
hupo <hupo@jiayukang.com>
2015-12-09 10:33:02 +0800

    init

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,19 @@ var extender = require('gulp-html-extend');
 var sass = require('gulp-ruby-sass');
 
 gulp.task('extend', function(){
-    gulp.src('build/*.html')
+    gulp.src('unpackage/*.html')
     .pipe(extender({annotations:false,verbose:false}))
     .pipe(gulp.dest('./'))
 });
 
 gulp.task('scss', function() {
-    return sass('build/scss',  { sourcemap: false })
+    return sass('unpackage/scss',  { sourcemap: false })
     .pipe(gulp.dest('css'));
 });
 
 gulp.task('watch', function() {
-    gulp.watch('build/*.html', ['extend']);
-    gulp.watch('build/scss/*.scss', ['scss']);
+    gulp.watch('unpackage/*.html', ['extend']);
+    gulp.watch('unpackage/scss/*.scss', ['scss']);
 });
 
 gulp.task('default', ['watch', 'scss', 'extend']);
